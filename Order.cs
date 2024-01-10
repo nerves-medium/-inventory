fe526c0355a94959497d64086704c269b72e817b
Katherine X <153394389+nerves-medium@users.noreply.github.com>
2024-01-10 13:02:42 +0700

    Create Order.cs

---
+++ Order.cs
@@ -0,0 +1,11 @@
+class Order
+{
+    public int OrderId { get; }
+    public ShoppingCart ShoppingCart { get; }
+
+    public Order(int orderId, ShoppingCart shoppingCart)
+    {
+        OrderId = orderId;
+        ShoppingCart = shoppingCart;
+    }
+}
