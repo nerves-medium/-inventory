18b40414cd28b1049b3af820523b00580c31af4e
Katherine X <153394389+nerves-medium@users.noreply.github.com>
2024-01-10 13:02:22 +0700

    Create Product.cs

---
+++ Product.cs
@@ -0,0 +1,11 @@
+class Product
+{
+    public string Name { get; }
+    public decimal Price { get; }
+
+    public Product(string name, decimal price)
+    {
+        Name = name;
+        Price = price;
+    }
+}
